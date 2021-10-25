acbb3f41ad8cd4740d0fecda40442413de5c08f3
Marcelo Kohlhase <marceloakohlhase@gmail.com>
2021-10-25 14:05:51 -0400

    Slide fotos

---
+++ 4 - Codigo/js/script.js
@@ -0,0 +1,21 @@
+if (window.SimpleSlide) {
+    new SimpleSlide({
+        slide: 'quote', // nome do atributo data-slide="principal"
+        time: 7000, // tempo de transição dos slides
+        // nav: true, // se deve ou não mostrar a navegação
+        // auto: true, // se o slide deve passar automaticamente
+        // pauseOnHover: true, // pausa a transição automática
+    })
+
+    new SimpleSlide({
+        slide: 'portfolio', // nome do atributo data-slide="principal"
+        time: 7000, // tempo de transição dos slides
+        nav: true, // se deve ou não mostrar a navegação
+        // auto: true, // se o slide deve passar automaticamente
+        // pauseOnHover: true, // pausa a transição automática
+    })
+}
+
+if (window.SimpleAnime) {
+    new.SimpleAnime;
+}
